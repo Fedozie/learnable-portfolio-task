f896a38abe1e29ba71189d292ddf02238bdfb342
Edozie <phynnedo@gmail.com>
2021-10-10 20:25:10 +0100

    Corrected bugs and set responsiveness for smaller devices

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ const navMenu = document.querySelector('.nav-menu');
 const themeIcon = document.querySelector('.theme-icon');
 const icon = document.querySelector('.material-icons');
 const buttons = document.querySelectorAll('button');
+const nextSlide = document.querySelector('.next-slide');
+const items = document.querySelectorAll('.item-link')
 
 //Event Listener for toggling menu bar on mobile devices
 menuBtn.addEventListener('click', () => {
     menuBtn.classList.toggle('active');
     navMenu.classList.toggle('active');
+    document.getElementsByTagName('body')[0].classList.toggle('stop-scrolling');
+    nextSlide.classList.toggle('hidden');
 })
 
 //Event Listener for theme change
@@ -21,4 +25,14 @@ themeIcon.addEventListener('click', () => {
         icon.style.color = "#CFCFCF";
         document.querySelector('body').classList.remove('light')
     }
+})
+
+//Event Listener for listening for click events for each item on the nav bar
+items.forEach((item) => {
+    item.addEventListener('click', () => {
+        menuBtn.classList.remove('active');
+        navMenu.classList.remove('active');
+        document.getElementsByTagName('body')[0].classList.remove('stop-scrolling');
+        nextSlide.classList.remove('hidden');
+    })
 })
